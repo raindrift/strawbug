35c1f2a07dd7396221ebef10d0f1943046856171
Ian Baker <ian@sonic.net>
2011-06-05 13:34:05 -0700

    added the redirect back into the edit view

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -158,7 +158,7 @@ class UsersController extends AppController {
 			
 			if ($this->User->save($this->data)) {
 				$this->Session->setFlash(__('The user has been saved', true));
-				#$this->redirect(array('action' => 'index'));
+				$this->redirect(array('action' => 'index'));
 			} else {
 				$this->Session->setFlash(__('The user could not be saved. Please, try again.', true));
 			}
